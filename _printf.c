3b9f8a5b2cfde8667c147e0d8874bacc80456968
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-14 10:11:56 -0800

    fixed

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 
 /**
  * _printf - Custom implementation of the printf function
- * supporting %c, %s, and %%
+ * supporting %c, %s, %d, %i, and %%
  * @format: A format string containing conversion specifiers
  * @...: Additional arguments corresponding to the conversion
  * specifiers in the format string
@@ -10,45 +10,58 @@
  * Return: The number of characters printed (excluding the
  * null byte at the end)
  */
+
 int _printf(const char *format, ...)
 {
-int printed_char;
-va_list args;
-va_start(args, format);
+	int printed_char;
+	va_list lst_args;
+	va_start(lst_args, format);
 
-printed_char = 0;
+	printed_char = 0;
 
-while (*format)
-{
-if (*format != '%')
-{
-write(1, format, 1);
-printed_char++;
-}
-else
-{
-format++;
-if (*format == '%')
-{
-write(1, "%", 1);
-printed_char++;
-}
-else if (*format == 'c')
-{
-int c = va_arg(args, int);
-write(1, &c, 1);
-printed_char++;
-}
-else if (*format == 's')
-{
-char *string = va_arg(args, char *);
-write(1, string, strlen(string));
-printed_char += strlen(string);
-}
-}
-format++;
-}
+	if (format==NULL || *format == '\0')
+	{
+		return (-1);
+	}
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			printed_char++;
+		}
+		else
+		{
+			format++;
+			if (*format == '%')
+			{
+				write(1, "%", 1);
+				printed_char++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(lst_args, int);
+				write(1, &c, 1);
+				printed_char++;
+			}
+			else if (*format == 's')
+			{
+				char *string = va_arg(lst_args, char *);
+				write(1, string, strlen(string));
+				printed_char += strlen(string);
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(lst_args, int);
+				char buffer[12];
+				int num_length = snprintf(buffer, sizeof(buffer), "%d", num);
+				write(1, buffer, num_length);
+				printed_char += num_length;
+			}
+		}
+		format++;
+	}
 
-va_end(args);
-return (printed_char);
+	va_end(lst_args);
+	return (printed_char);
 }
