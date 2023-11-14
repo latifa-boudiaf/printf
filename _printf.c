115c527f39e9775bb3316c3cbe327be215da2df7
imeneHamida <imene.hamida18@gmail.com>
2023-11-14 07:17:55 -0800

    task 0 fixing bugs

--- _printf.c
+++ _printf.c
@@ -13,9 +13,12 @@
 int _printf(const char *format, ...)
 {
 int printed_char;
-va_list args;
-va_start(args, format);
+va_list lst_args;
 
+if (format==NULL || *format == '\0')
+return -1;
+
+va_start(lst_args, format);
 printed_char = 0;
 
 while (*format)
@@ -35,13 +38,14 @@ printed_char++;
 }
 else if (*format == 'c')
 {
-int c = va_arg(args, int);
+char c = va_arg(lst_args, int);
 write(1, &c, 1);
 printed_char++;
 }
 else if (*format == 's')
 {
-char *string = va_arg(args, char *);
+char *string = va_arg(lst_args, char *);
+
 write(1, string, strlen(string));
 printed_char += strlen(string);
 }
@@ -49,6 +53,6 @@ printed_char += strlen(string);
 format++;
 }
 
-va_end(args);
+va_end(lst_args);
 return (printed_char);
 }
